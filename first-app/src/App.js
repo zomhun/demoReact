e4bc2bfb3800da8a4311d11f8d0575d26e7ee95f
zomhun <datpt1997@gmail.com>
2022-10-05 15:43:36 +0700

    new file:   first-app/public/css/bootstrap-grid.css 	new file:   first-app/public/css/bootstrap-grid.css.map 	new file:   first-app/public/css/bootstrap-grid.min.css 	new file:   first-app/public/css/bootstrap-grid.min.css.map 	new file:   first-app/public/css/bootstrap-grid.rtl.css 	new file:   first-app/public/css/bootstrap-grid.rtl.css.map 	new file:   first-app/public/css/bootstrap-grid.rtl.min.css 	new file:   first-app/public/css/bootstrap-grid.rtl.min.css.map 	new file:   first-app/public/css/bootstrap-reboot.css 	new file:   first-app/public/css/bootstrap-reboot.css.map 	new file:   first-app/public/css/bootstrap-reboot.min.css 	new file:   first-app/public/css/bootstrap-reboot.min.css.map 	new file:   first-app/public/css/bootstrap-reboot.rtl.css 	new file:   first-app/public/css/bootstrap-reboot.rtl.css.map 	new file:   first-app/public/css/bootstrap-reboot.rtl.min.css 	new file:   first-app/public/css/bootstrap-reboot.rtl.min.css.map 	new file:   first-app/public/css/bootstrap-utilities.css 	new file:   first-app/public/css/bootstrap-utilities.css.map 	new file:   first-app/public/css/bootstrap-utilities.min.css 	new file:   first-app/public/css/bootstrap-utilities.min.css.map 	new file:   first-app/public/css/bootstrap-utilities.rtl.css 	new file:   first-app/public/css/bootstrap-utilities.rtl.css.map 	new file:   first-app/public/css/bootstrap-utilities.rtl.min.css 	new file:   first-app/public/css/bootstrap-utilities.rtl.min.css.map 	new file:   first-app/public/css/bootstrap.css 	new file:   first-app/public/css/bootstrap.css.map 	new file:   first-app/public/css/bootstrap.min.css 	new file:   first-app/public/css/bootstrap.min.css.map 	new file:   first-app/public/css/bootstrap.rtl.css 	new file:   first-app/public/css/bootstrap.rtl.css.map 	new file:   first-app/public/css/bootstrap.rtl.min.css 	new file:   first-app/public/css/bootstrap.rtl.min.css.map 	modified:   first-app/public/index.html 	new file:   first-app/public/js/bootstrap.bundle.js 	new file:   first-app/public/js/bootstrap.bundle.js.map 	new file:   first-app/public/js/bootstrap.bundle.min.js 	new file:   first-app/public/js/bootstrap.bundle.min.js.map 	new file:   first-app/public/js/bootstrap.esm.js 	new file:   first-app/public/js/bootstrap.esm.js.map 	new file:   first-app/public/js/bootstrap.esm.min.js 	new file:   first-app/public/js/bootstrap.esm.min.js.map 	new file:   first-app/public/js/bootstrap.js 	new file:   first-app/public/js/bootstrap.js.map 	new file:   first-app/public/js/bootstrap.min.js 	new file:   first-app/public/js/bootstrap.min.js.map 	modified:   first-app/src/App.js 	new file:   first-app/src/Components/Demo.js 	modified:   first-app/src/index.js

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-group">
+      <form>
+        <div className="form-group">
+          <label htmlFor="formControlRange">Example Range input</label>
+          <input type="range" className="form-control-range" id="formControlRange" />
+        </div>
+      </form>
     </div>
   );
 }
---
+++ first-app/src/Components/Demo.js
@@ -0,0 +1,7 @@
+function Demo() {
+  return (
+    <h3>abcsdsdccc</h3>
+  );
+}
+
+export default Demo;
